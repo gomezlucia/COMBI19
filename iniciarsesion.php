bf0e61970cb60730f8766f0ce89ce6363553e647
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-04-28 12:15:29 -0300

    Update iniciarsesion.php

--- iniciarsesion.php
+++ iniciarsesion.php
@@ -1,13 +1,13 @@
 <?php
-	include "BD.php";
+	include "COMBI19/BD.php";
 	$link = conectar();
-	include "validarLogin.php";
+	include "COMBI19/validarLogin.php";
 	$usuario= new usuario();
 	try {
 		$usuario -> validar_usuario($link); 
-		header("Location: /home.php"); //si los datos del usuario son correctos lo dirige a la pag principal
+		header("Location: /COMBI19/home.php"); //si los datos del usuario son correctos lo dirige a la pag principal
 	} catch (Exception $e){
 		$mensaje= $e->getMessage(); //obtiene todas las excepciones que se pudieon haber producido y las guarda en una variable
-		header("Location: /inicioSesion.php?mensaje=$mensaje");//redirige a la pag inicioSesion con el/los mensajes pasador en la url (metodo GET)
+		header("Location: /COMBI19/inicioSesion.php?mensaje=$mensaje");//redirige a la pag inicioSesion con el/los mensajes pasador en la url (metodo GET)
 	}
 ?>
