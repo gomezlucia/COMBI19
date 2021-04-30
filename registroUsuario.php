c191f66d2cc3c7e870b64693c87f98de9e45fa25
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-04-29 21:47:05 -0300

    Update and rename registrousuario.php to registroUsuario.php

---
+++ registroUsuario.php
@@ -0,0 +1,20 @@
+  <html>
+  <head>
+    <title>Registro de Usuarios</title>
+  </head>
+  <body>
+    <form action="registrarse.php" method="post">
+     <h1> Registrar usuario </h1>   
+				<input type="text" name="nombre" size=50 placeholder=" Nombre"> <br><br>          
+				<input type="text" name="apellido" size=50 placeholder=" Apellido"> <br><br>
+				<input type="text" name="fecha_nacimiento" size=50 placeholder=" Fecha de nacimiento"> <br><br>
+				<input type="text" name="DNI" size=50 placeholder=" DNI"> <br><br>
+				<input type="email" name="mail" size=50 placeholder=" Email"> <br><br>          
+				<input type="text" name="nombre_usuario" size=50 placeholder=" Nombre de usuario"> <br><br>
+				<input type="password" name="contraseña" size=50 placeholder=" Contraseña"> <br><br>
+				<input type="submit" value="Guardar">
+				<input type= "reset" value= "Borrar">
+    </form>
+  </body>
+</html>
+	
