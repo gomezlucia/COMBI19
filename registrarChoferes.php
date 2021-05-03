d1d3c6b7236c2e4dd0c0751b42bf6667e672a418
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-05-03 14:37:07 -0300

    Update registrarChoferes.php

--- registrarChoferes.php
+++ registrarChoferes.php
@@ -8,8 +8,8 @@
 				<input type="text" name="nombre" size=50 placeholder=" Nombre"> <br><br>
 				<input type="text" name="apellido" size=50 placeholder=" Apellido"> <br><br>
 				<input type="text" name="nombre_usuario" size=50 placeholder=" Nombre de usuario"> <br><br>
-				<input type="text" name="contraseña" size=50 placeholder=" Contraseña"> <br><br>
-        <input type="text" name="clave1" size=50   minlength= "8 " placeholder=" Confirmar Contraseña"> <br><br>
+				<input type="password" name="contraseña" size=50 placeholder=" Contraseña"> <br><br>
+        <input type="password" name="clave1" size=50   minlength= "8 " placeholder=" Confirmar Contraseña"> <br><br>
 
 				<input type="submit" value="Guardar">
 				<input type= "reset" value= "Borrar">
