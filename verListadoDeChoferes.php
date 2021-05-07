1604514037bfc8ae4f5c18023d008bb9bfa6e244
mailen912 <62101563+mailen912@users.noreply.github.com>
2021-05-07 13:22:22 -0300

    Add files via upload

--- verListadoDeChoferes.php
+++ verListadoDeChoferes.php
@@ -9,12 +9,15 @@
 </head>
 <body>
      <?php
-        $consulta= "SELECT SEC_TO_TIME(sum(time_to_sec(TIMEDIFF(v.fecha_hora_llegada, v.fecha_hora_salida)))) as horas, v.id_chofer,u.nombre, u.apellido FROM viajes v inner join usuarios u on(v.id_chofer=u.id_usuario) WHERE(now()>=+v.fecha_hora_salida) GROUP BY v.id_chofer" ;
+        $consulta= "SELECT SEC_TO_TIME(sum(time_to_sec(TIMEDIFF(v.fecha_hora_llegada, v.fecha_hora_salida)))) as horas, v.id_chofer,u.nombre, u.apellido, u.mail, u.legajo FROM viajes v inner join usuarios u on(v.id_chofer=u.id_usuario) WHERE(now()>=+v.fecha_hora_salida) GROUP BY v.id_chofer UNION SELECT 0 AS horas, u1.id_usuario,u1.nombre, u1.apellido, u1.mail, u1.legajo FROM usuarios u1 WHERE(u1.tipo_usuario='chofer')and NOT EXISTS ( SELECT * from viajes v1 WHERE v1.id_chofer=u1.id_usuario)" ;
         $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
+
         if ($resultado){
         	while (($valores = mysqli_fetch_array($resultado)) ){
         	 $nombre = $valores['nombre'];
              $apellido = $valores['apellido'];
+             $mail = $valores['mail'];
+             $legajo = $valores['legajo'];
              $horas_trabajadas = $valores['horas'];
              ?>
              	<div>
@@ -22,6 +25,8 @@
              		<p>
              			<b>Nombre:</b> <?php echo $nombre;?><br>
              			<b>Apellido:</b> <?php echo $apellido;?><br>
+                        <b>Mail:</b> <?php echo $mail;?><br>
+                        <b>Legajo:</b> <?php echo $legajo;?><br>
                         <b>Horas trabajadas:</b> <?php  
                         $horas=explode(":", $horas_trabajadas);
                         echo $horas[0];
