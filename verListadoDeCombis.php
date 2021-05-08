0fe55b06335436e1a9bfdbdb0c6249c23eadb1aa
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-05-08 20:32:55 -0300

    Update verListadoDeCombis.php

--- verListadoDeCombis.php
+++ verListadoDeCombis.php
@@ -9,11 +9,12 @@
 	<h1>Combis</h1>
 </head>
 <body>
+
      <?php
-        $consulta= "SELECT id_combi, patente, chasis, modelo, nombre_tipo FROM combis NATURAL JOIN tipos_combi WHERE debaja = 0" ;#debaja = 0 es falso
+        $consulta= "SELECT id_combi, patente, chasis, modelo, nombre_tipo FROM combis NATURAL JOIN tipos_combi " ;#debaja = 0 es falso
         $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
         if ($resultado){
-        	while (($valores = mysqli_fetch_array($resultado)) ){
+                  	while (($valores = mysqli_fetch_array($resultado)) ){
         	 $patente = $valores['patente'];
              $chasis = $valores['chasis'];
              $modelo = $valores['modelo'];
@@ -27,29 +28,43 @@
              			<b>Numero de chasis:</b> <?php echo $chasis;?><br>
              			<b>Modelo:</b> <?php echo $modelo;?><br>
              			<b>Tipo de combi:</b> <?php echo $tipo_combi;?><br>
-                  <label for="myCheck">Dar de baja combi    </label>
-                  <input type="checkbox" id="myCheck" onclick="evaluarDebaja($id_combi)">
-                  <?php echo $mensaje; ?>
+                  <input type="checkbox" onclick="llamarDebaja();">Dar de baja combi<br><br>
+
 
              		</p>
                 <?php
-                function evaluarDebaja($id_combi){
-                  $mensaje= "La combi fue dada de baja exitosamente" ;
-                  $consulta= "SELECT v.origen, v.id_combi, v.fecha_hora_salida, c.debaja FROM viajes v NATURAL JOIN combis c WHERE (now()<=fecha_hora_salida)" ;
-                  $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
-                  if ($resultado){
-                  	while (($valores = mysqli_fetch_array($resultado)) ){
-                       $id_combi_act = $valores['id_combi'];
-                       if ($id_combi_act == $id_combi){
-                          $mensaje= "Esta combi esta asignada a un viaje. Por favor, modifique o cancele el viaje antes de dar de baja la combi";
-                       }
-                     }
-                     return $mensaje;
-                   }
-                 }
 
-                 ?>
+               function evaluarDebaja($id_combi){
+                 $mensaje = 'La combi fue dada de baja exitosamente';
+               //  echo="funcion";
+                 $consulta= "SELECT v.origen, v.id_combi, v.fecha_hora_salida FROM viajes v  WHERE (now()<=fecha_hora_salida)" ;
+                 $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
+                 if ($resultado){
+                   while (($valores = mysqli_fetch_array($resultado)) ){
+                      $id_combi_act = $valores['id_combi'];
+                      if ($id_combi_act == $id_combi){
+                      //  $mensaje= 'No se puede dar de baja las combis que tengan viajes asignados, por favor primero modifique los datos de esos viajes o cancele el viaje en caso de que tenga pasajes vendidos';
+                          echo 'hola';
+                      }
+                    }
+                  }
+                    echo $mensaje;
 
+                }
+               ?>
+
+               <?php
+               function imprimir($mensaje){ echo $mensaje ; } ?>
+
+
+
+
+                <script>
+                function llamarDebaja(){
+                  alert('<?php echo evaluarDebaja($id_combi); ?>');
+                }
+
+                </script>
 
              	</div>
 
@@ -57,6 +72,7 @@
              <?php
 
         }
+
         if(mysqli_num_rows($resultado)==0){
             ?>
             <div>
@@ -65,7 +81,7 @@
             </div>
             <?php
 
-    }
+            }
        }
 
         ?>
