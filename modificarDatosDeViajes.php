32e71a37df23577c8eece5ef5c8f8259fa65582d
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-05-09 16:56:04 -0300

    Update modificarDatosDeViajes.php

--- modificarDatosDeViajes.php
+++ modificarDatosDeViajes.php
@@ -14,17 +14,17 @@
    <?php 
    // try {
     //	$usuario -> iniciada($usuarioID);
-    $consulta="SELECT id_combi,id_chofer,origen,destino,precio,fecha_hora_salida,fecha_hora_llegada FROM viajes WHERE id_viaje=1 ";
+    $id_viaje=$_POST['id_viaje'];
+    $consulta="SELECT id_combi,id_chofer,origen,destino,precio,fecha_hora_salida,fecha_hora_llegada FROM viajes WHERE id_viaje='$id_viaje' ";
     $resultado=mysqli_query($link,$consulta) or  die ('Consulta fallida: ' .mysqli_error());
     $viaje=mysqli_fetch_array ($resultado); 
     $origen=$viaje['origen'];
     $destino=$viaje['destino'];
-    $precio=$viaje['precio'];
+    $precio= $viaje['precio'];
     $id_combi=$viaje['id_combi'];
     $id_chofer=$viaje['id_chofer'];
     $fecha_hora_salida=$viaje['fecha_hora_salida'];
     $fecha_hora_llegada=$viaje['fecha_hora_llegada'];
-    $id_viaje=1;
 
   ?><center>
      <h2>Modificar datos del viaje</h2>
@@ -33,20 +33,20 @@
 	   Origen  <input type="text"  name="origen"  placeholder="Origen viaje" size=50 value="<?php echo $origen; ?>" required ></input><br><br>  
 		 Destino <input type ="text" name="destino" size=50 placeholder="Destino viaje" value="<?php echo $destino; ?>" required></input><br><br> 
 		 Precio $ <input type ="number" name="precio" size=50 placeholder="Precio viaje" value="<?php echo $precio; ?>" required></input><br><br> 
-		 <?php $consulta= "SELECT id_combi,patente,chasis,modelo FROM combis WHERE id_combi not in (SELECT id_combi from viajes WHERE not ('$fecha_hora_salida' BETWEEN fecha_hora_salida and fecha_hora_llegada) or ('$fecha_hora_llegada' BETWEEN fecha_hora_salida and fecha_hora_llegada))";
+		 <?php $consulta= "SELECT id_combi,patente,chasis,modelo FROM combis WHERE id_combi not in (SELECT id_combi from viajes WHERE  ('$fecha_hora_salida' BETWEEN fecha_hora_salida and fecha_hora_llegada) or ('$fecha_hora_llegada' BETWEEN fecha_hora_salida and fecha_hora_llegada))";
           $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
-	       if (mysqli_num_rows($resultado)==0){ ?>
-               <p>No hay combis disponibles</P>
+	        if (mysqli_num_rows($resultado)==0){ ?>
+               <p>No hay combis disponibles en ese horario</P>
 		 <?php    } else { ?>
 		 <select name= 'combis'>
              <option value="0">Seleccione combi</option>
              <?php while ($valores = mysqli_fetch_array($resultado)) {
                  echo '<option value="' . $valores["id_combi"] . '">' . $valores["patente"] ." ". $valores["chasis"]."  ". $valores["modelo"] .  '</option>';}?>
          </select> <br><br> <?php } ?>
-         <?php $consulta= "SELECT id_usuario,nombre,apellido FROM usuarios WHERE tipo_usuario='chofer' and id_usuario not in (SELECT id_chofer from viajes WHERE not ('$fecha_hora_salida' BETWEEN fecha_hora_salida and fecha_hora_llegada) or ('$fecha_hora_llegada' BETWEEN fecha_hora_salida and fecha_hora_llegada))";
+         <?php $consulta= "SELECT id_usuario,nombre,apellido FROM usuarios WHERE tipo_usuario='chofer' and id_usuario not in (SELECT id_chofer from viajes WHERE  ('$fecha_hora_salida' BETWEEN fecha_hora_salida and fecha_hora_llegada) or ('$fecha_hora_llegada' BETWEEN fecha_hora_salida and fecha_hora_llegada))";
           $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
 	      if (mysqli_num_rows($resultado)==0){ ?>
-               <p>No hay choferes disponibles</P>
+               <p>No hay choferes disponibles en ese horario</P>
 		 <?php    } else { ?>
 		 <select name= 'choferes'>
              <option value="0">Seleccione chofer</option>
