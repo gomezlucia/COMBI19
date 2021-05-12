0b9e584eb3b2b93763cd360a05baf3ee7c9db1f4
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-05-11 21:03:40 -0300

    Update verListadoDeCombis.php

--- verListadoDeCombis.php
+++ verListadoDeCombis.php
@@ -1,7 +1,7 @@
-
 <?php
     include("BD.php");// conectar y seleccionar la base de datos
     $link=conectar();
+//    include("funcionEvaluarDebaja.php");
 
 ?>
 <!DOCTYPE html>
@@ -11,85 +11,56 @@
 </head>
 <body>
 
+
      <?php
-        $consulta= "SELECT id_combi, patente, chasis, modelo, nombre_tipo, debaja, asientos FROM combis NATURAL JOIN tipos_combi " ;#debaja = 0 es falso
+        $consulta= "SELECT id_combi, patente, chasis, modelo, nombre_tipo, debaja FROM combis NATURAL JOIN tipos_combi " ;#debaja = 0 es falso
         $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
         if ($resultado){
           while (($valores = mysqli_fetch_array($resultado)) ){
+
         	 $patente = $valores['patente'];
              $chasis = $valores['chasis'];
              $modelo = $valores['modelo'];
              $id_combi = $valores['id_combi'];
              $tipo_combi = $valores['nombre_tipo'] ;
              $debaja = $valores['debaja'];
-             $asientos=$valores['asientos'];
              ?>
              <div>
+
              	<h2><?php echo $patente ?></h2>
              	<div>
              		<p>
              			<b>Numero de chasis:</b> <?php echo $chasis;?><br>
              			<b>Modelo:</b> <?php echo $modelo;?><br>
              			<b>Tipo de combi:</b> <?php echo $tipo_combi;?><br>
-                  <b>Cantidad de asientos:</b> <?php echo $asientos;?><br>
+                  <b>Tipo de combi555555555:</b> <?php echo $id_combi;?><br>
                 <?php  if ($debaja == 0){
                 //    $tipo='checkbox';
                 ?><br>
-                <input type="checkbox" onclick="llamarDebaja();">Dar de baja combi<br><br>
+                  <form action="funcionEvaluarDebaja.php" method="post">
+                <input type="hidden" name="id_combi" value="<?php echo $id_combi; ?>"> </input>
+                <input type="submit" value="Dar de baja"><br><br></input>
+
+                </form>
                 <?php
               }
               else{ ?>
                 <br>
                 <b >Combi dada de baja <br><br>
-              <?php }} ?>
-
-
-
+              <?php } }?>
 
              		</p>
-                <?php
-
-               function evaluarDebaja($id_combi){
-                 $mensaje = 'La combi fue dada de baja exitosamente';
-               //  echo="funcion";
-
-                $consulta2= "SELECT id_combi FROM viajes WHERE (now()<=fecha_hora_salida) and id_combi ='$id_combi'" ;
-               $result25= mysqli_query ($link, $consulta2) or die ('Consulta fallida 44' .mysqli_error());
-               echo $mensaje;
-            //       if (mysqli_num_rows($resultado)>0){
-            //     if ($resultado){
-            //      while (($valores = mysqli_fetch_array($resultado)) ){
-              //        $id_combi_act = $valores['id_combi'];
-              //        if ($id_combi_act == $id_combi){
-              //           $mensaje= 'No se puede dar de baja las combis que tengan viajes asignados, por favor primero modifique los datos de esos viajes o cancele el viaje en caso de que tenga pasajes vendidos';
+              <?php
 
 
-
-
-
-
-                }
                ?>
 
-               <?php
-               function imprimir($mensaje){ echo $mensaje ; } ?>
-
-
-
-
-                <script>
-                function llamarDebaja(){
-                  alert('<?php echo evaluarDebaja($id_combi); ?>');
-                }
-
-                </script>
-
-             	</div>
+              	</div>
 
              </div>
              <?php
 
-        
+
 
         if(mysqli_num_rows($resultado)==0){
             ?>
@@ -103,3 +74,5 @@
        }
 
         ?>
+</html>
+
