d3fc55653bf6c6c5b0ce79c9340c849b00bb9521
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-05-12 19:28:43 -0300

    Update registroUsuario.php

--- registroUsuario.php
+++ registroUsuario.php
@@ -3,11 +3,12 @@
     <title>Registro de Usuarios</title>
   </head>
   <body>
+	  <a href="home.php">Volver al home</a>
     <form action="registrarse.php" method="post">
      <h1> Registrar usuario </h1>
 				<input type="text" name="nombre" size=50 placeholder=" Nombre"> <br><br>
 				<input type="text" name="apellido" size=50 placeholder=" Apellido"> <br><br>
-				<input type="date" name="fecha_nacimiento" size=50 placeholder=" Fecha de nacimiento"> <br><br>
+				Fecha de nacimiento <input type="date" name="fecha_nacimiento" size=50 placeholder=" Fecha de nacimiento"> <br><br>
 				<input type="text" name="DNI" size=50 placeholder=" DNI sin puntos"> <br><br>
 				<input type="email" name="mail" size=50 placeholder=" Email"> <br><br>
 				<input type="text" name="nombre_usuario" size=50 placeholder=" Nombre de usuario"> <br><br>
