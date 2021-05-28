75ce2caecd101d422debaf796477c4ac47ba597c
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-05-28 16:45:57 -0300

    Update registrarse.php

--- registrarse.php
+++ registrarse.php
@@ -2,266 +2,128 @@
 	include("BD.php");// conectar y seleccionar la base de datos
 	$link = conectar();
 	$cumple= false;
-	$cumple1= false;
+	$cumple1=false;
 	$mensaje= "";
-	$contra_correcta= true;
+	$exito=false;
 	session_start();
 
+	 function edad($edad){
+         list($anio,$mes,$dia) = explode("-",$edad);
+         $anio_dif = date("Y") - $anio;
+         $mes_dif = date("m") - $mes;
+         $dia_dif = date("d") - $dia;
+         if ($dia_dif < 0 || $mes_dif < 0) { 
+         	$anio_dif--; 
+         } 
+     return $anio_dif; }
 
-	function edad($edad){
-   list($anio,$mes,$dia) = explode("-",$edad);
-    $anio_dif = date("Y") - $anio;
-    $mes_dif = date("m") - $mes;
-    $dia_dif = date("d") - $dia;
-     if ($dia_dif < 0 || $mes_dif < 0) { $anio_dif--; } return $anio_dif; }
+     function validarContra($contra){
+     	 if ((strlen($contra)) >= 8){
+			 $mayus= 0;
+			 $simbolo= 0;
+			 $nro= 0;
+			 for ($i=0; $i<strlen($contra);$i++){
+				 $caracter= $contra[$i];
+				 if (ctype_upper($caracter)){
+					 $mayus ++;
+				 }elseif (!ctype_alnum ($caracter)) {
+					 $simbolo ++;
+					 } elseif (ctype_digit($caracter)){
+						 $nro ++;
+					 }
+			 }
+	         if (($mayus <> 0) and (($simbolo <> 0) and ($nro <> 0)))  {
+			 	 return true;
+			 }else{
+			 	return false;
+			 }
+	     }else{
+	     	return false;
+	     }
+     }
 
-  if ($_POST['tipo_usuario']=='cliente'){
+     function existeMail($mail,$link){
+         $estaMail="SELECT mail FROM usuarios WHERE mail='$mail'";//hacer consulta
+		 $resultado= mysqli_query ($link, $estaMail) or die ('Consulta estamail fallida ' .mysqli_error());
+		 $num=mysqli_num_rows($resultado);
+     return $num;}
 
-	if ((isset ($_POST['nombre'])) and (isset ($_POST['apellido']))) {
-		$nombre= $_POST['nombre'];
-		$apellido= $_POST['apellido'];
-		if((!empty($_POST['nombre'])) and (!empty($_POST['apellido'])) and (!empty($_POST['nombre_usuario'])) and (!empty($_POST['mail'])) and (!empty($_POST['contraseña'])) and (!empty($_POST['fecha_nacimiento']) and
-    (!empty($_POST['DNI'])) and (!empty($_POST['clave1'])))){
-		if ((ctype_alpha($nombre)) and (ctype_alpha($apellido))){// verificacion si contiene solo caracteres alfabeticos
-			if (isset ($_POST['nombre_usuario'])){
-				$usuario= $_POST['nombre_usuario'];
-//				if ((ctype_alnum($usuario))) {// alfanumerico y 6 caracteres
-					if ((isset ($_POST['contraseña'])) and (isset ($_POST['clave1']))){
-						if($_POST['clave1']==$_POST['contraseña']) {
-						$contra= $_POST['contraseña'];
-						$clave= $_POST['clave1'];
-						if ((strlen($contra)) >= 8){
-							$mayus= 0;
-							$simbolo= 0;
-							$nro= 0;
-							for ($i=0; $i<strlen($contra);$i++){
-								$caracter= $contra[$i];
-								if (ctype_upper($caracter)){
-									$mayus ++;
-								} elseif (!ctype_alnum ($caracter)) {
-									$simbolo ++;
-								} elseif (ctype_digit($caracter)){
-									$nro ++;
-								}
-							}
+     function existeNombreUsuario($nombre,$link){
+         $estaNombreU= "SELECT nombre_usuario FROM usuarios WHERE nombre_usuario='$nombre'";//hacer consulta
+		 $resultado= mysqli_query ($link, $estaNombreU) or die ('Consulta estaNombreU fallida' .mysqli_error());
+		 $num=mysqli_num_rows($resultado);
+	 return $num;}
 
-							 if (($mayus <> 0) and (($simbolo <> 0) or ($nro <> 0))) {
+	 function existeLegajo($legajo,$link){
+         $estaLegajo= "SELECT legajo FROM usuarios WHERE legajo='$legajo'";//hacer consulta
+		 $resultado= mysqli_query ($link, $estaLegajo) or die ('Consulta estaLegajo fallida' .mysqli_error());
+		 $num=mysqli_num_rows($resultado);
+     return $num;}
 
-										if(isset($_POST['mail'])){
-											$mail=$_POST['mail'];
+     if(isset($_POST['nombre']) and isset($_POST['apellido']) and isset($_POST['mail']) and isset($_POST['nombre_usuario']) and isset($_POST['contraseña']) and isset($_POST['clave1'])  ){
+       	 $nombre= $_POST['nombre'];
+		 $apellido= $_POST['apellido'];
+		 $usuario= $_POST['nombre_usuario'];
+		 $contraseña= $_POST['contraseña'];
+		 $mail=$_POST['mail'];
+		 $nombre_usuario=$_POST['nombre_usuario'];
+		 if ((ctype_alpha($nombre)) and (ctype_alpha($apellido))){	 	
+		 	 if($_POST['clave1']==$contraseña) {
+				 if(validarContra($contraseña)){
+				     if(existeNombreUsuario($nombre_usuario,$link)==0){	     
+                         if(existeMail($mail,$link)==0){
+                             $cumple=true;
+                         }else{
+                             $mensaje="El mail ingresado ya tiene una cuenta asociada";
+                         }
+				     }else{
+                         $mensaje="El nombre de usuario ya existe, por favor elija otro"; 
+				     }         
+				 }else{
+				 	 $mensaje="La contraseña debe contar con al menos 8 caracteres, contener letras mayúsculas, letras minúsculas, poseer al menos un número y un símbolo";
+				 }
+			 }else{  
+			 	 $mensaje="Las contraseñas no coinciden";
+			 }			 
+		 }else{ 
+		 	 $mensaje= "El nombre y el apellido deben ser alfabeticos";
+		 }
+     }
 
-                      if((ctype_digit($_POST['DNI'])) and ((strlen($_POST['DNI'])) >= 7) and ((strlen($_POST['DNI'])) < 9)){
-												$dni=$_POST['DNI'];
-
-												if ( edad($_POST['fecha_nacimiento']) >= 18){
-													$fecha=$_POST['fecha_nacimiento'];
-											  $cumple1= true;
-											}
-											else{
-												$mensaje='Debes ser mayor de edad para registrarte en esta pagina';
-											}
-                       }
-                       else{
-                         $mensaje="DNI invalido. Por favor intente de nuevo";
-                       }
-										}
-										else{
-										$mensaje="debe introducir un mail";
-										}
-									}
-							 else {
-									$contra_correcta= false;
-							}
-						} else {
-							$contra_correcta= false;
-						}
-					 } else {
-						 $mensaje="Las contraseñas no coinciden";
-						}
-					}else{
-                         $contra_correcta= false;
-					}
-//				} else {
-//					$mensaje="ingrese";
-//				}
-			} else {
-				$mensaje= "Introduzca un nombre de usuario";
-			}
-		} else {
-			$mensaje= "El nombre y el apellido deben ser alfanumericos";
-		}
-		} else{
-			$mensaje="Complete todos los campos";
-		}
-	} else {
-		$mensaje= "Introduzca su nombre y apellido";
+ if ($_POST['tipo_usuario']=='cliente'){ 
+     $dni=$_POST['DNI'];
+     $fecha_nacimiento=$_POST['fecha_nacimiento'];
+     if((ctype_digit($dni)) and ((strlen($dni)) >= 7) and ((strlen($dni)) < 9)){
+         if ( edad($fecha_nacimiento) >= 18){
+		     $cumple1= true;
+	     }else{
+			 $mensaje='Debes ser mayor de edad para registrarte en esta pagina';
+		 }
+     }else{
+         $mensaje="DNI invalido. Por favor intente de nuevo";
+     }
+ }else{ //es chofer
+     $legajo=$_POST['legajo'];
+     if(existeLegajo($legajo,$link)==0){
+         $cumple1= true;
+	}else{
+		$mensaje='Ya existe un usuario con ese legajo por favor ingrese otro';
 	}
-}
-////////////////////SI ES CHOFER SE COMPLETRA EL OTRO REGISTRO
-else{
-  if ((isset ($_POST['nombre'])) and (isset ($_POST['apellido']))) {
-    $nombre= $_POST['nombre'];
-    $apellido= $_POST['apellido'];
-    if((!empty($_POST['nombre'])) and (!empty($_POST['apellido'])) and (!empty($_POST['nombre_usuario'])) and (!empty($_POST['mail'])) and (!empty($_POST['legajo'])) and (!empty($_POST['contraseña'])) and
-		(!empty($_POST['clave1']))){
-    if ((ctype_alpha($nombre)) and (ctype_alpha($apellido))){// verificacion si contiene solo caracteres alfabeticos
-      if (isset ($_POST['nombre_usuario'])){
-        $usuario= $_POST['nombre_usuario'];
-//				if ((ctype_alnum($usuario))) {// alfanumerico y 6 caracteres
-          if ((isset ($_POST['contraseña'])) and (isset ($_POST['clave1']))){
+ }
 
-          if($_POST['clave1']==$_POST['contraseña']) {
-            $contra= $_POST['contraseña'];
-            if ((strlen($contra)) >= 8){
-              $mayus= 0;
-              $simbolo= 0;
-              $nro= 0;
-              for ($i=0; $i<strlen($contra);$i++){
-                $caracter= $contra[$i];
-                if (ctype_upper($caracter)){
-                  $mayus ++;
-                } elseif (!ctype_alnum ($caracter)) {
-                  $simbolo ++;
-                } elseif (ctype_digit($caracter)){
-                  $nro ++;
-                }
-              }
+	 if ($cumple1 and $cumple){
+          if ($_POST['tipo_usuario']=='cliente'){
+		     $agregarCliente= "INSERT INTO usuarios (apellido, nombre, mail, nombre_usuario, contraseña, fecha_nacimiento, DNI, tipo_usuario) values ('$apellido', '$nombre', '$mail', '$nombre_usuario', '$contraseña', '$fecha_nacimiento','$dni', 'cliente' )";
+		     $resultado= mysqli_query ($link, $agregarCliente) or die ('Consuluta agregarCliente fallida : ' .mysqli_error($link));
+		     $exito= true;
+	     }else{
+             $agregarChofer= "INSERT INTO usuarios (apellido, nombre, nombre_usuario, mail, legajo, contraseña, tipo_usuario, debaja) values ('$apellido', '$nombre', '$nombre_usuario', '$mail', '$legajo', '$contraseña', 'chofer' , '0')";
+		     $resultado= mysqli_query ($link, $agregarChofer) or die ('Consuluta agregarCliente fallida : ' .mysqli_error($link));
+		     $exito= true;
+         }
+     }
 
-              if (($mayus <> 0) and (($simbolo <> 0) and ($nro <> 0))) {
-								if(isset($_POST['mail'])){
-									$mail=$_POST['mail'];
-									if(isset($_POST['legajo'])){
-										$legajo=$_POST['legajo'];
-                  $cumple1= true;
-								    }
-										else{
-											$mensaje= "Introduzca legajo del chofer";
-										}
-								}
-								else{
-									$mensaje="Introduzca email";
-								}
-                    }
-               else {
-                $contra_correcta= false;
-              }
-            } else {
-              $contra_correcta= false;
-            }
-           } else {
-            $mensaje="Las contraseñas no coinciden";
-          }
-      }
-          else{
-          	$contra_correcta= false;
-          }
-//				} else {
-//					$mensaje="ingrese";
-//				}
-      } else {
-        $mensaje= "Introduzca un nombre de usuario";
-      }
-    } else {
-      $mensaje= "El nombre y el apellido deben ser alfanumericos";
-    }
-    } else{
-      $mensaje="Complete todos los campos";
-    }
-  } else {
-    $mensaje= "Introduzca su nombre y apellido";
-  }
-}
-
-if ($contra_correcta == false ){
-	$mensaje= "La contraseña debe contar con al menos 8 caracteres, contener letras mayúsculas, letras minúsculas, poseer al menos un número y un símbolo";
-$error=true;
-$_SESSION['nombre_formulario'] = $_POST['nombre'];
-		$_SESSION['apellido_formulario'] = $_POST['apellido'];
-				$_SESSION['fecha_formulario'] = $_POST['fecha_nacimiento'];
-				$_SESSION['dni_formulario'] = $_POST['DNI'];
-				$_SESSION['nombre_usuario_formulario'] = $_POST['nombre_usuario'];
-				$_SESSION['contra_formulario'] = $_POST['contraseña'];
-				$_SESSION['mail_formulario'] = $_POST['mail'];
-				$_SESSION['legajo_formulario'] = $_POST['legajo'];
-}
-
-
-
-	$cumple2=true;// verificacion de la existencia del nombre de usuario
-	if ($cumple1 == true){
-		$query25= ("SELECT nombre_usuario FROM usuarios");//hacer consulta
-		$result25= mysqli_query ($link, $query25) or die ('Consulta fallida 70' .mysqli_error());
-		while ($usuarioTabla= mysqli_fetch_array ($result25)){
-			if ($usuario == $usuarioTabla['nombre_usuario']){
-				$cumple2= false;
-				$mensaje="El nombre de usuario ya existe, por favor elija otro";
-				$error=true;
-				$_SESSION['nombre_formulario'] = $_POST['nombre'];
-						$_SESSION['apellido_formulario'] = $_POST['apellido'];
-								$_SESSION['fecha_formulario'] = $_POST['fecha_nacimiento'];
-								$_SESSION['dni_formulario'] = $_POST['DNI'];
-								$_SESSION['nombre_usuario_formulario'] = $_POST['nombre_usuario'];
-								$_SESSION['contra_formulario'] = $_POST['contraseña'];
-								$_SESSION['mail_formulario'] = $_POST['mail'];
-								$_SESSION['legajo_formulario'] = $_POST['legajo'];
-			}
-		}
-	}
-	if (($cumple1 == true) and ($_POST['tipo_usuario'] <> 'cliente')){
-		$query25= ("SELECT legajo FROM usuarios");//hacer consulta
-		$result25= mysqli_query ($link, $query25) or die ('Consulta fallida 170' .mysqli_error());
-		while ($usuarioTabla= mysqli_fetch_array ($result25)){
-			if ($_POST['legajo'] == $usuarioTabla['legajo']){
-				$cumple2= false;
-				$mensaje='Ya existe un usuario con ese legajo por favor ingrese otro';
-				$error=true;
-				$_SESSION['nombre_formulario'] = $_POST['nombre'];
-						$_SESSION['apellido_formulario'] = $_POST['apellido'];
-												$_SESSION['legajo_formulario'] = $_POST['legajo'];
-								$_SESSION['nombre_usuario_formulario'] = $_POST['nombre_usuario'];
-								$_SESSION['contra_formulario'] = $_POST['contraseña'];
-								$_SESSION['mail_formulario'] = $_POST['mail'];
-			}
-		}
-	}
-
-	if ($cumple1 == true){
-        $query27= ("SELECT mail FROM usuarios");//hacer consulta
-        $mail= $_POST['mail'];
-		$result27= mysqli_query ($link, $query27) or die ('Consulta fallida 183' .mysqli_error());
-		while ($usuarioTabla= mysqli_fetch_array ($result27)){
-			if ($mail == $usuarioTabla['mail']){
-				$cumple2= false;
-				$mensaje="El mail ingresado ya tiene una cuenta asociada";
-				$error=true;
-				$_SESSION['nombre_formulario'] = $_POST['nombre'];
-						$_SESSION['apellido_formulario'] = $_POST['apellido'];
-								$_SESSION['fecha_formulario'] = $_POST['fecha_nacimiento'];
-								$_SESSION['dni_formulario'] = $_POST['DNI'];
-								$_SESSION['nombre_usuario_formulario'] = $_POST['nombre_usuario'];
-								$_SESSION['contra_formulario'] = $_POST['contraseña'];
-								$_SESSION['mail_formulario'] = $_POST['mail'];
-								$_SESSION['legajo_formulario'] = $_POST['legajo'];
-			}
-		}
-	}
-
-
-	if (($cumple1== true) and ($cumple2== true)){
-    if ($_POST['tipo_usuario']=='cliente'){
-		$query31= "INSERT INTO usuarios (apellido, nombre, mail, nombre_usuario, contraseña, fecha_nacimiento, DNI, tipo_usuario) values ('$_POST[apellido]', '$_POST[nombre]', '$_POST[mail]', '$_POST[nombre_usuario]', '$_POST[contraseña]', '$_POST[fecha_nacimiento]','$_POST[DNI]', 'cliente' )";//falta subir la imagen y su tipo
-		$result31= mysqli_query ($link, $query31) or die ('Consuluta query1 fallida 163: ' .mysqli_error($link));
-		$exito= true;
-	}
-  else{
-    $query31= "INSERT INTO usuarios (apellido, nombre, nombre_usuario, mail, legajo, contraseña, tipo_usuario, debaja) values ('$_POST[apellido]', '$_POST[nombre]', '$_POST[nombre_usuario]', '$_POST[mail]', '$_POST[legajo]', '$_POST[contraseña]', 'chofer' , '0')";//falta subir la imagen y su tipo
-		$result31= mysqli_query ($link, $query31) or die ('Consuluta query1 fallida 168: ' .mysqli_error($link));
-		$exito= true;
-  }
-}
-
-
- if ((isset($exito)) and (($exito==true))){
+ if ((isset($exito)) and ($exito)){
 	 if($_POST['tipo_usuario']=='cliente'){
          echo "<script > alert('Usuario regitrado exitosamente');window.location='inicioSesion.php'</script>";
      }
@@ -270,10 +132,22 @@ $_SESSION['nombre_formulario'] = $_POST['nombre'];
      }
  }else {
 	 if($_POST['tipo_usuario']=='cliente'){
-
-         echo "<script > alert('Error al completar el formulario.'+'$mensaje');window.location='registroUsuario.php?error=$error'</script>";
+	 	 $_SESSION['nombre_cliente']=$nombre;
+	 	 $_SESSION['apellido_cliente']=$apellido;
+	 	 $_SESSION['nombre_usuario_cliente']=$nombre_usuario;
+	 	 $_SESSION['mail_cliente']=$mail;
+	 	 $_SESSION['contraseña_cliente']=$contraseña;
+         $_SESSION['fecha_nacimiento_c']=$fecha_nacimiento;
+         $_SESSION['DNI_c']=$dni;
+         echo "<script > alert('$mensaje');window.location='registroUsuario.php?error=$exito'</script>";
      }else{
-     	 echo "<script > alert('Error al completar el formulario.'+'$mensaje');window.location='registrarChoferes.php?error=$error'</script>";
+     	 $_SESSION['nombre_chofer']=$nombre;
+	 	 $_SESSION['apellido_chofer']=$apellido;
+	 	 $_SESSION['nombre_usuario_chofer']=$nombre_usuario;
+	 	 $_SESSION['contraseña_chofer']=$contraseña;
+	 	 $_SESSION['mail_chofer']=$mail;
+	 	 $_SESSION['legajo_chofer']=$legajo;
+     	 echo "<script > alert('$mensaje');window.location='registrarChoferes.php?error=$exito'</script>";
      }
   }
 
