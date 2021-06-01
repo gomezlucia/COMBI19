77fde3e96e664193571829a72fee37ed895ac4b5
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-05-31 23:47:47 -0300

    Update calificarViaje.php

--- calificarViaje.php
+++ calificarViaje.php
@@ -14,11 +14,10 @@
 <body>
   <?php  try {
             $usuario -> iniciada($nombreUsuario); //entra al body si el usuario tenia una sesion iniciada
-
  ?>
 
  <a href="home.php" >Volver al home </a>
- <form action="calificarViaje.php" method="post">
+ <form action="validarCalificacion.php" method="post">
   <p class="clasificacion">
     <input id="radio1" type="radio" name="estrella" value="1"><!--
     --><label for="radio1">★</label><!--
@@ -31,11 +30,12 @@
     --><input id="radio5" type="radio" name="estrella" value="5"><!--
     --><label for="radio5">★</label>
     <br><br>
-    
 
-    <h1>¡Dejanos un comnetario sobre el viaje!</h1>
-      <input type="text" name="comentario" size="50" maxlength="50">
-      <br><br>
+
+    <h1>¡Dejanos un comentario sobre el viaje!</h1>
+      <textarea name="comentario" size="2000" minlength="50"></textarea>
+      <input type="hidden" name="id_viaje" value="<?php echo $_POST['id_viaje']; ?>">
+      <input type="hidden" name="id" value="<?php echo $id; ?>">
       <br><br>
       <input type="submit" value="Guardar">
       <input type= "reset" value= "Cancelar">
@@ -43,6 +43,14 @@
   </p>
 </form>
 
+<?php  //echo "<script> alert('$_POST['estrella']');window.location='/COMBI19-main/calificarViaje.php'</script>";
+ } catch (Exception $e) { //entra a esta parte solo si no tenia una sesion iniciada
+                 $mensaje=$e->getMessage();
+                 echo "<script> alert('$mensaje');window.location='/COMBI19-main/inicioSesion.php'</script>";
+                  //redirige a la pagina inicioSesion y muestra una mensaje de error
+     }
+     ?>
+</html>
 
 
 
