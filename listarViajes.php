66a866837ad53f95162d94419c91b54276ca48bf
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-06-01 15:06:26 -0300

    Update listarViajes.php

--- listarViajes.php
+++ listarViajes.php
@@ -5,22 +5,25 @@
      $usuario= new usuario();
      $usuario -> session ($nombreUsuario); //guarda en $nombreUsuario el valor que tiene la sesion (lo pasa por referencia)
      $usuario ->id($id);
+     $sesion=true;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 </head>
-     <?php try {
-             $usuario -> iniciada($nombreUsuario); //entra al body si el usuario tenia una sesion iniciada
-     ?> 
+
 <body>
 <a href="home.php">Volver al home</a>
       <h1>Viajes disponibles</h1>
      <?php
-         $consulta="SELECT tipo_usuario FROM usuarios WHERE id_usuario='$id'"; 
-         $resultado=mysqli_query($link,$consulta) or  die ('Consulta fallida: ' .mysqli_error());
-         $usuario=mysqli_fetch_array ($resultado); 
-         $tipo_usuario=$usuario['tipo_usuario'];
+          
+         $usuario -> tieneSesionIniciada($sesion,$nombreUsuario);
+         if($sesion){
+             $consulta="SELECT tipo_usuario FROM usuarios WHERE id_usuario='$id'"; 
+             $resultado=mysqli_query($link,$consulta) or  die ('Consulta fallida: ' .mysqli_error());
+             $usuario=mysqli_fetch_array ($resultado);
+             $tipo_usuario=$usuario['tipo_usuario'];
+         }
          $consulta= "SELECT v.id_viaje,v.id_ruta,r.origen, r.destino, v.fecha_hora_salida, v.fecha_hora_salida, v.fecha_hora_llegada, v.precio, v.cupo, t.asientos FROM viajes v NATURAL JOIN combis c NATURAL JOIN tipos_combi t NATURAL JOIN rutas r WHERE (now()<=fecha_hora_salida)and (debaja=0)" ;
          $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
          if ($resultado){
@@ -46,23 +49,30 @@
                          echo $cupo ?> <i>(está lleno)</i>
                      <?php }?><br>
                  </p>
-                 <?php 
-                 if( ($cupo==0) and ($tipo_usuario=='administrador') ){ ?>
-                     <form action="modificarDatosDeViajes.php" method="post">
-                         <input type="submit" name="modificar" value="Modificar Viaje"></input>
-                         <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
-                     </form> 
-             <?php } 
+                 <?php if($sesion){  
+                         if( ($cupo==0) and ($tipo_usuario=='administrador') ){ ?>
+                             <form action="modificarDatosDeViajes.php" method="post">
+                                 <input type="submit" name="modificar" value="Modificar Viaje"></input>
+                                 <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
+                             </form> 
+             <?php       }elseif (($cupo<$asientos) and ($tipo_usuario=='cliente') ) { ?>
+                             <form action="comprarPasaje.php" method="post">
+                                 <input type="submit" name="comprar" value="Comprar viaje"></input>
+                                 <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
+                             </form> 
+             <?php       }
+                     }elseif (($cupo<$asientos) ) { ?>
+                         <form action="comprarPasaje.php" method="post">
+                             <input type="submit" name="comprar" value="Comprar viaje"></input>
+                             <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
+                         </form> 
+<?php                }
              }
-             if(mysqli_num_rows($resultado)==0){ ?>
-                 <center> <b>No hay viajes disponibles por el momento</b> </center>
-<?php        }
-         }     ?>
+                 
+        }
+         if(mysqli_num_rows($resultado)==0){ ?>
+             <center> <b>No hay viajes disponibles por el momento</b> </center>
+<?php        }   ?>
 </body>
- <?php   } catch (Exception $e) { //entra a esta parte solo si no tenia una sesion iniciada
-                 $mensaje=$e->getMessage(); 
-                 echo "<script> alert('$mensaje');window.location='/COMBI19-main/inicioSesion.php'</script>";
-                  //redirige a la pagina inicioSesion y muestra una mensaje de error
-         }?>
 </html>
 
