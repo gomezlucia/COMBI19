916745e57c283091788049def03d5b5e945b4dcc
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-06-02 15:34:37 -0300

    Add files via upload

---
+++ comprarPasaje.php
@@ -0,0 +1,69 @@
+<?php
+     include("BD.php");// conectar y seleccionar la base de datos
+     $link=conectar();
+     include "validarLogin.php";
+     $usuario= new usuario();
+     $usuario -> session ($nombreUsuario); //guarda en $nombreUsuario el valor que tiene la sesion (lo pasa por referencia)
+     $usuario ->id($id);
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Comprar Pasaje</title>
+ </head>
+  <?php try {
+             $usuario -> iniciada($nombreUsuario); //entra al body si el usuario tenia una sesion iniciada
+     ?> 
+ <body>
+    <?php 
+       
+         $noTieneCovid="SELECT c.tiene_covid ,t.numero_tarjeta, t.codigo_seguridad, t.fecha_vencimiento FROM usuarios c INNER JOIN tarjetas_clientes tc ON (c.id_usuario=tc.id_cliente) INNER JOIN tarjetas t ON (tc.id_tarjeta= t.id_tarjeta) Where id_usuario='$id'";
+         $resultado=mysqli_query($link,$noTieneCovid) or  die ('Consulta noTieneCovid fallida: ' .mysqli_error());
+         $cliente=mysqli_fetch_array($resultado);
+         if($cliente['tiene_covid']!=0) {
+             echo "<script > alert('No puede comprar este viaje ya que ha declarado síntomas compatibles con COVID-19 dentro de los 15 días anteriores a la fecha de salida');window.location='listarViajes.php'</script>";
+         }else{
+         	 if (!isset($_POST['id_viaje'])) {
+         	 	 $id_viaje=$_SESSION['viaje'];
+         	 	 $numero_tarjeta=$_SESSION['tarjeta'];
+         	 }else{
+         	     $id_viaje=$_POST['id_viaje'];
+         	     $numero_tarjeta="";
+         	 }
+         	 $viaje="SELECT  r.origen, r.destino, v.fecha_hora_salida, v.fecha_hora_salida, v.fecha_hora_llegada, v.precio FROM viajes v  NATURAL JOIN rutas r  WHERE id_viaje='$id_viaje'";
+         	 $resultadoViaje=mysqli_query($link,$viaje) or  die ('Consulta viaje fallida: ' .mysqli_error()); 
+         	 $datosViaje = mysqli_fetch_array($resultadoViaje);
+         	 $adicionales="SELECT vs.id_servicio_adicional, vs.id_viaje,s.nombre_servicio,s.precio FROM servicios_adicionales s NATURAL JOIN viajes_servicios_adicionales vs WHERE vs.id_viaje='$id_viaje'";
+         	 $resultado=mysqli_query($link,$adicionales) or  die ('Consulta noTieneCovid fallida: ' .mysqli_error()); ?>
+         	 <center>
+         	 	 <p> <b>Origen:</b> <?php echo $datosViaje['origen'];?><br>
+         		     <b>Destino:</b> <?php echo $datosViaje['destino'];?><br>
+             	     <b>Fecha y hora de salida:</b> <?php echo  date("d/m/Y  H:i:s", strtotime($datosViaje['fecha_hora_salida']));?><br>
+                     <b>Fecha y hora de llegada:</b> <?php echo date("d/m/Y  H:i:s", strtotime($datosViaje['fecha_hora_llegada']));?><br>
+                     <b>Precio:</b> <?php echo '$' . $datosViaje['precio'];?><br>
+                     <b>Seleccionar adicionales:<b><br>
+                     <form action="validarPago.php" method="post">  
+                     <?php   while ($valores = mysqli_fetch_array($resultado)) {
+                     	         echo '<input type="checkbox" name="chkl[]" value="' . $valores["id_servicio_adicional"] . '">' . $valores["nombre_servicio"] ." $". $valores["precio"].   '</input><br><br>';
+                     	         }?>
+                     <b>Numero de tarjeta: </b><input type="number" name="numero_tarjeta"  maxlength="16" required="" value="<?php echo $numero_tarjeta?>"></input><br><br>
+                     <b>Clave de seguridad: </b><input type="password" name="clave"  maxlength="4" required="" value=""></input><br><br>
+                          <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
+                          <input type="submit" name="Submit" value="Pagar pasaje"> 
+                          <input type="hidden" name="precio" value="<?php echo  $datosViaje['precio']; ?>">
+                          <input type="reset" name="cancelar" value="Cancelar"></input><br><br>
+              
+                     </form>
+         	 </center>
+ <?php  } 
+    
+    ?>
+ 
+ </body>
+ <?php   } catch (Exception $e) { //entra a esta parte solo si no tenia una sesion iniciada
+                 $mensaje=$e->getMessage(); 
+                 echo "<script> alert('Por favor, inicie sesión en COMBI-19 o regístrese como nuevo usuario para realizar la compra');window.location='/COMBI19-main/inicioSesion.php'</script>";
+                  //redirige a la pagina inicioSesion y muestra una mensaje de error
+         }?>
+</html>
+          
---
+++ validarPago.php
@@ -0,0 +1,83 @@
+<?php
+     include("BD.php");// conectar y seleccionar la base de datos
+     $link=conectar();
+     include "validarLogin.php";
+     $usuario= new usuario();
+     $usuario -> session ($nombreUsuario); //guarda en $nombreUsuario el valor que tiene la sesion (lo pasa por referencia)
+     $usuario ->id($id); 
+  $tarjetaValida=false;
+ // $mensaje="";
+
+  if( (!empty($_POST['numero_tarjeta'])) and (!empty ($_POST['clave'])) ) {
+     $numero=$_POST['numero_tarjeta'];
+     $codigo_seguridad=$_POST['clave'];
+     $primero = substr($numero, 0,1); 
+     $primeros = substr($numero, 0,2); 
+     if(($primeros==37) or ($primeros==34) or ($primero==4) or ($primero==5)){
+         if(($primeros==37) or ($primeros==34)){
+             if((strlen($numero)==15)){
+                 if((strlen($codigo_seguridad)==4) ){
+                     $tarjetaValida=true;
+                 }//else{ $mensaje="el codigo de seguridad no es valido"; } 
+             }//else{ $mensaje="el numero de tarjeta no es valido";    }
+         }else{
+             if(strlen($numero)==16){
+                 if((strlen($codigo_seguridad)==3)){
+                     $tarjetaValida=true;
+                 }//else{ $mensaje="el codigo de seguridad no es valido"; }
+             }//else{  $mensaje="el numero de tarjeta no es valido"; }
+         } 
+     }//else{ $mensaje="Solo se aceptan las siguientes tarjetas: american express, mastercard y visa"; }
+ } 
+if (substr($numero,-1,1)!=3) {
+	 if ($tarjetaValida) {
+	     if (substr($numero,-1,1)!=8) {
+	     	 if (!empty($_POST['chkl'])) {
+               	 $checkbox1 = $_POST['chkl'];
+                 $adicionalesSeleccionados=""; 
+                 $precioAdicional=0;
+                 foreach($checkbox1 as $valor) { 
+                     $consulta="SELECT nombre_servicio,precio FROM servicios_adicionales WHERE id_servicio_adicional='$valor'";  
+                     $resultado=mysqli_query($link,$consulta)  or die ('Consulta fallida: ' .mysqli_error());
+                     $valores = mysqli_fetch_array($resultado);
+                     $adicionalesSeleccionados=$valores['nombre_servicio']."/".$adicionalesSeleccionados;
+                     $precioAdicional=$precioAdicional+$valores['precio'];
+  		         } 
+  			     $precio=$precioAdicional+ $_POST['precio'];
+  			     $consulta="INSERT INTO clientes_viajes(id_cliente, id_viaje, estado, servicios_adicionales, tarjeta_utilizada, total) VALUES ('$id','$_POST[id_viaje]','pendiente','$adicionalesSeleccionados','$numero','$precio')";	 
+                 $resultado=mysqli_query($link,$consulta) or  die ('Consulta fallida: ' .mysqli_error()); 
+                 $actualizarCupo="UPDATE viajes SET cupo=cupo+1 WHERE id_viaje='$_POST[id_viaje]'";
+                 $resultado=mysqli_query($link,$actualizarCupo) or  die ('Consulta actualizarCupo fallida: ' .mysqli_error()); 
+
+                 echo "<script> alert('Compra exitosa');window.location='/COMBI19-main/listarViajes.php'</script>";
+             }
+	 	     $consulta="INSERT INTO clientes_viajes(id_cliente, id_viaje, estado,tarjeta_utilizada, total) VALUES ('$id','$_POST[id_viaje]','pendiente','$numero','$_POST[precio]')";
+	 	     $resultado=mysqli_query($link,$consulta) or  die ('Consulta fallida: ' .mysqli_error()); 
+             $actualizarCupo="UPDATE viajes SET cupo=cupo+1 WHERE id_viaje='$_POST[id_viaje]'";
+             $resultado=mysqli_query($link,$actualizarCupo) or  die ('Consulta actualizarCupo fallida: ' .mysqli_error()); 
+             echo "<script> alert('Compra exitosa');window.location='/COMBI19-main/listarViajes.php'</script>";
+	     }else{
+	     	 $_SESSION['tarjeta']=$numero;
+	     	 $_SESSION['viaje']=$_POST['id_viaje'];
+             echo "<script> alert('La tarjeta no posee saldo suficiente');window.location='/COMBI19-main/comprarPasaje.php'</script>";
+	     } 
+     }else{
+     	 $_SESSION['tarjeta']=$numero;
+	     $_SESSION['viaje']=$_POST['id_viaje'];
+	     echo "<script> alert('Numero de tarjeta invalido');window.location='/COMBI19-main/comprarPasaje.php'</script>";
+     }
+}else{ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<a href="home.php">Volver al home</a>
+ <center> 
+ 	 <h2>No se pudo establecer la conexión con el servidor</h1>
+ 	 <a href="">Por favor vuelva a intertalo</a>
+ </center>
+</body>
+</html>
+<?php } ?>
