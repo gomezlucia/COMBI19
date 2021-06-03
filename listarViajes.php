51f1efb2b9ed30b67e9fd8126b5642e74b0eef5a
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-06-03 13:34:15 -0300

    Update listarViajes.php

--- listarViajes.php
+++ listarViajes.php
@@ -1,55 +1,39 @@
-<?php
-     include("BD.php");// conectar y seleccionar la base de datos
-     $link=conectar();
-     include "validarLogin.php";
-     $usuario= new usuario();
-     $usuario -> session ($nombreUsuario); //guarda en $nombreUsuario el valor que tiene la sesion (lo pasa por referencia)
-     $usuario ->id($id);
-     $sesion=true;
-?>
-<!DOCTYPE html>
-<html>
-<head>
-</head>
 
-<body>
-<a href="home.php">Volver al home</a>
-      <h1>Viajes disponibles</h1>
-     <?php
-          
-         $usuario -> tieneSesionIniciada($sesion,$nombreUsuario);
-         if($sesion){
-             $consulta="SELECT tipo_usuario FROM usuarios WHERE id_usuario='$id'"; 
-             $resultado=mysqli_query($link,$consulta) or  die ('Consulta fallida: ' .mysqli_error());
-             $usuario=mysqli_fetch_array ($resultado);
-             $tipo_usuario=$usuario['tipo_usuario'];
-         }
-         $consulta= "SELECT v.id_viaje,v.id_ruta,r.origen, r.destino, v.fecha_hora_salida, v.fecha_hora_salida, v.fecha_hora_llegada, v.precio, v.cupo, t.asientos FROM viajes v NATURAL JOIN combis c NATURAL JOIN tipos_combi t NATURAL JOIN rutas r WHERE (now()<=fecha_hora_salida)and (debaja=0)" ;
-         $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
-         if ($resultado){
-        	while (($valores = mysqli_fetch_array($resultado)) ){
-        	     $origen = $valores['origen'];
-                 $destino = $valores['destino'];
-                 $fecha_hora_salida = $valores['fecha_hora_salida'];
-                 $fecha_hora_llegada = $valores['fecha_hora_llegada'] ;
-                 $precio = $valores['precio'] ;
-                 $cupo=$valores['cupo'];
-                 $asientos=$valores['asientos'];
-                 $id_viaje=$valores['id_viaje'];?>
-                 <hr>
-                 <p> <b>Origen:</b> <?php echo $origen;?><br>
-         		     <b>Destino:</b> <?php echo $destino;?><br>
-             	     <b>Fecha y hora de salida:</b> <?php echo  date("d/m/Y  H:i:s", strtotime($fecha_hora_salida));?><br>
-                     <b>Fecha y hora de llegada:</b> <?php echo  date("d/m/Y  H:i:s", strtotime($fecha_hora_llegada));?><br>
-                     <b>Precio:</b> <?php echo '$' . $precio;?><br>
-                     <b>Cupo:</b> <?php 
-                     if($cupo<$asientos){
-                         echo $cupo;
-                     }else{
-                         echo $cupo ?> <i>(está lleno)</i>
-                     <?php }?><br>
-                 </p>
-                 <?php if($sesion){  
+     <?php function listarViajes($link,$usuario,$nombreUsuario){
+             $sesion=true;
+             $usuario -> tieneSesionIniciada($sesion,$nombreUsuario);
+              if($sesion){
+                 $consulta="SELECT tipo_usuario FROM usuarios WHERE id_usuario='$id'"; 
+                 $resultado=mysqli_query($link,$consulta) or  die ('Consulta fallida: ' .mysqli_error());
+                 $usuario=mysqli_fetch_array ($resultado);
+                 $tipo_usuario=$usuario['tipo_usuario'];
+             }
+             $consulta= "SELECT v.id_viaje,v.id_ruta,r.origen, r.destino, v.fecha_hora_salida, v.fecha_hora_salida, v.fecha_hora_llegada, v.precio, v.cupo, t.asientos FROM viajes v NATURAL JOIN combis c NATURAL JOIN tipos_combi t NATURAL JOIN rutas r WHERE (now()<=fecha_hora_salida)and (debaja=0)" ;
+             $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
+             if ($resultado){
+                 while (($valores = mysqli_fetch_array($resultado)) ){
+                     $origen = $valores['origen'];
+                     $destino = $valores['destino'];
+                     $fecha_hora_salida = $valores['fecha_hora_salida'];
+                     $fecha_hora_llegada = $valores['fecha_hora_llegada'] ;
+                     $precio = $valores['precio'] ;
+                     $cupo=$valores['cupo'];
+                     $asientos=$valores['asientos'];
+                     $id_viaje=$valores['id_viaje']; ?>
+                      <p> 
+                         <b>Origen:</b> <?php echo $origen;?><br>
+                         <b>Destino:</b> <?php echo $destino;?><br>
+                         <b>Fecha y hora de salida:</b> <?php echo  date("d/m/Y  H:i:s", strtotime($fecha_hora_salida));?><br>
+                         <b>Fecha y hora de llegada:</b> <?php echo  date("d/m/Y  H:i:s", strtotime($fecha_hora_llegada));?><br>
+                         <b>Precio:</b> <?php echo '$' . $precio;?><br>
+                         <b>Cupo:</b> <?php 
+                         if($cupo<$asientos){
+                             echo $cupo;
+                         }else{
+                             echo $cupo ?> <i>(está lleno)</i>
+                   <?php }   ?>
+                     <br></p>
+               <?php if($sesion){  
                          if( ($cupo==0) and ($tipo_usuario=='administrador') ){ ?>
                              <form action="modificarDatosDeViajes.php" method="post">
                                  <input type="submit" name="modificar" value="Modificar Viaje"></input>
@@ -58,7 +42,7 @@
              <?php       }elseif (($cupo<$asientos) and ($tipo_usuario=='cliente') ) { 
                              $comproPasaje="SELECT * FROM clientes_viajes WHERE id_viaje='$id_viaje' and id_cliente='$id'";
                              $resultadoPasaje= mysqli_query($link,$comproPasaje) or die ('Consulta comproPasaje fallida: ' .mysqli_error($link));
-                             if(mysqli_num_rows($resultadoPasaje)==0){?>
+                             if(mysqli_num_rows($resultadoPasaje)==0){ ?>
                                  <form action="comprarPasaje.php" method="post">
                                      <input type="submit" name="comprar" value="Comprar pasaje"></input>
                                      <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
@@ -75,13 +59,13 @@
                              <input type="submit" name="comprar" value="Comprar viaje"></input>
                              <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
                          </form> 
-<?php                }
-             }
-                 
-        }
-         if(mysqli_num_rows($resultado)==0){ ?>
-             <center> <b>No hay viajes disponibles por el momento</b> </center>
-<?php        }   ?>
-</body>
-</html>
+<?php                }   ?>
+                 <hr>
+<?php                } // while (($valores = mysqli_fetch_array($resultado)) )
+
+             } //  if ($resultado)
+             if(mysqli_num_rows($resultado)==0){ ?>
+                 <center> <b>No hay viajes disponibles por el momento</b> </center>
+<?php        }  
+     } ?>
 
