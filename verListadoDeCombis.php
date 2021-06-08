ce724c46818a3654cbb8402819146135fac2ba47
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-06-08 15:39:32 -0300

    Update verListadoDeCombis.php

--- verListadoDeCombis.php
+++ verListadoDeCombis.php
@@ -1,15 +1,29 @@
 <?php
     include("BD.php");// conectar y seleccionar la base de datos
     $link=conectar();
+        include "validarLogin.php";
+    $usuario= new usuario();
+    $usuario -> session ($nombreUsuario); //guarda en $nombreUsuario el valor que tiene la sesion (lo pasa por referencia)
+    $usuario ->id($id);
+    include "menu.php";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<h1>Combis</h1>
+ <link rel="stylesheet" type="text/css" href="estilos.css" media="all" > </link>
 </head>
 <body>
 
- <a href="home.php" >Volver al home </a>   
+ <header>
+       <a href="home.php" >  
+           <img src="logo_is.png" class="div_icono">  
+       </a>
+       <b><?php echo $nombreUsuario; ?></b>
+<?php           echo menu($id,$link); ?>                       
+       <hr>     
+     </header> 
+     <center> 
+    <h1>Combis</h1>
      <?php
         $consulta= "SELECT id_combi, patente, chasis, modelo, nombre_tipo, debaja, asientos FROM combis NATURAL JOIN tipos_combi " ;#debaja = 0 es falso
         $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: ' .mysqli_error($link));
@@ -24,51 +38,29 @@
              $debaja = $valores['debaja'];
              $asientos = $valores['asientos'];
              ?>
-             <div>
-
-             	<h2><?php echo $patente ?></h2>
-             	<div>
+             	<h3><?php echo $patente ?></h3>
              		<p>
              			<b>Numero de chasis:</b> <?php echo $chasis;?><br>
              			<b>Modelo:</b> <?php echo $modelo;?><br>
              			<b>Tipo de combi:</b> <?php echo $tipo_combi;?><br>
                   <b>Cantidad de asientos:</b> <?php echo $asientos;?><br>
-                <?php  if ($debaja == 0){
-                //    $tipo='checkbox';
-                ?><br>
-                  <form action="funcionEvaluarDebaja.php" method="post">
-                <input type="hidden" name="id_combi" value="<?php echo $id_combi; ?>"> </input>
-                <input type="hidden" name="tipo" value="combi"> </input>
-                <input type="submit" value="Dar de baja"><br><br></input>
-
-                </form>
-                <?php
-              }
-              else{ ?>
-                <br>
-                <b >Combi dada de baja <br><br>
-              <?php } }?>
-
-             		</p>
-              <?php
- ?>
-           	</div>
-
-             </div>
-             <?php
-
-
-
-        if(mysqli_num_rows($resultado)==0){
-            ?>
-            <div>
+                <?php  if ($debaja == 0){ ?>
+                      <form action="funcionEvaluarDebaja.php" method="post">
+                         <input type="hidden" name="id_combi" value="<?php echo $id_combi; ?>"> </input>
+                          <input type="hidden" name="tipo" value="combi"> </input>
+                         <input type="submit" value="Dar de baja"><br><br></input>
+                     </form>
+                <?php }else{ ?>
+                     <b >Combi dada de baja <br><br>
+              <?php  } 
+         }?>
+                 </p>
+<?php        if(mysqli_num_rows($resultado)==0){ ?>
                  <p>
-                  <center> <b>Aun no hay combis cargadas en la pagina</b>
-            </div>
-            <?php
-
-            }
-       }
-
-        ?>
+                  <b>Aun no hay combis cargadas en la pagina</b>
+                 </p>
+<?php        }
+         } ?>
+        </center>  
 </html>
+
