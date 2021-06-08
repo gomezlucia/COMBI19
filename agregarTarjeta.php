93a8654b270c769d9a1d23eb39b4b5e5ce702936
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-06-08 11:16:14 -0300

    Update agregarTarjeta.php

--- agregarTarjeta.php
+++ agregarTarjeta.php
@@ -5,14 +5,26 @@
     $usuario= new usuario();
     $usuario -> session ($nombreUsuario); //guarda en $nombreUsuario el valor que tiene la sesion (lo pasa por referencia)
     $usuario ->id($id);
+    include "menu.php";
+
 ?>
+
   <html>
   <head>
     <title>Agregar tarjeta</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css" media="all" > </link>
   </head>
   <body>
+    <header>
+       <a href="home.php" >  
+           <img src="logo_is.png" class="div_icono">  
+       </a>
+       <b><?php echo $nombreUsuario; ?></b>
+<?php           echo menu($id,$link); ?>                       
+       <hr>     
+     </header>
  <?php $fcha = date("Y-m");?>
-  <a href="home.php" >Volver al home </a>     
+    <center>
     <form action="validarDatosTarjeta.php" method="post">
      <h1>Nueva tarjeta </h1>   
         <input type="text" name="numero" size=50 placeholder="Numero de tarjeta" ><br><br>          
@@ -23,7 +35,7 @@
         <input type= "reset" value= "Borrar">
         <input type="button" name="Cancelar" value="Cancelar" onClick="location.href='verPerfilDeUsuario.php'"> 
     </form>
-
+</center>
 
   </body>
 </html>
