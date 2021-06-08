f9577e34db02ef4a31ca7b4cda449ad8ce7818a7
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-06-08 15:42:54 -0300

    Update verListadoDeRutas.php

--- verListadoDeRutas.php
+++ verListadoDeRutas.php
@@ -1,14 +1,27 @@
 <?php
     include("BD.php");// conectar y seleccionar la base de datos
     $link=conectar();
+        include "validarLogin.php";
+    $usuario= new usuario();
+    $usuario -> session ($nombreUsuario); //guarda en $nombreUsuario el valor que tiene la sesion (lo pasa por referencia)
+    $usuario ->id($id);
+    include "menu.php";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+     <link rel="stylesheet" type="text/css" href="estilos.css" media="all" > </link>
 </head>
 <body>
-
- <a href="home.php" >Volver al home </a>
+  <header>
+       <a href="home.php" >  
+           <img src="logo_is.png" class="div_icono">  
+       </a>
+       <b><?php echo $nombreUsuario; ?></b>
+<?php           echo menu($id,$link); ?>                       
+       <hr>     
+     </header>
+     <center>
  <h2>Rutas</h2>
      <?php
         $consulta= "SELECT id_ruta, origen,destino,debaja FROM rutas " ;
@@ -26,15 +39,18 @@
                  <form action="modificarRuta.php" method="post">
                      <input type="submit" name="modificar" value="Modificar Ruta"></input>
                      <input type="hidden" name="ruta" value="<?php echo $id_ruta; ?>"></input>
-                 </form>
+                 </form><br>
                  <form action="darDebajaRuta.php" method="post">
                      <input type="submit" name="debaja" value="Dar de baja Ruta"></input>
                      <input type="hidden" name="ruta" value="<?php echo $id_ruta; ?>"></input>
                  </form>
              </p><hr>
-              <?php }
-             if(mysqli_num_rows($resultado)==0){?>
-                 <center> Aun no hay rutas cargadas</center>
-            <?php } }?>
+    <?php }
+          if(mysqli_num_rows($resultado)==0){?>
+              <p><b>Aun no hay rutas cargadas</b></p>
+            <?php } 
+         }?>
+         </center>
 </body>
 </html>
+
