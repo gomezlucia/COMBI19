a679c18e0e4f0376dff722331d3c1f93f66fa331
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-06-09 00:16:26 -0300

    Update darDebajaRuta.php

--- darDebajaRuta.php
+++ darDebajaRuta.php
@@ -8,56 +8,39 @@
     $usuario ->id($id);
     $debaja = false;
     $id_ruta_original=$_POST['ruta'];
-    echo $id_ruta_original."<br>";
     $consulta0="SELECT id_viaje  FROM viajes WHERE (id_ruta= '$id_ruta_original')";
     $result0=mysqli_query ($link, $consulta0) or die ('Consuluta query1 fallida 10: ' .mysqli_error($link));
-    echo 'hola1';
-    if ($result0){
-        echo '3';
-        var_dump($result0);
-        while($valores = mysqli_fetch_array($result0)){
+      if ($result0){
+          while($valores = mysqli_fetch_array($result0)){
           $id_viaje=$valores['id_viaje'];
-          echo '4';
-    //      echo date("Y"), date("m"), date("d");
+          //      echo date("Y"), date("m"), date("d");
         //  echo $hoy;
         $consulta2="SELECT estado FROM clientes_viajes WHERE (id_viaje= '$id_viaje')";
         $result2=mysqli_query ($link, $consulta2) or die ('Consuluta query1 fallida 12: ' .mysqli_error($link));
-        echo '5';
         while (($valores1 = mysqli_fetch_array($result2)) and ($debaja== false)){
-          echo '6';
           if ($valores1['estado']=='en curso'){
-              echo "<script > alert('La ruta no puede ser eliminada ya que hay un viaje en curso por dicha ruta');window.location='listarRutas.php'</script>";
+              echo "<script > alert('La ruta no puede ser eliminada ya que hay un viaje en curso por dicha ruta');window.location='verListadoDeRutas.php'</script>";
           } else {
-          echo '7';
-          $debaja= true;
+        $debaja= true;
         }
       }
       }
 } else {
   $consulta1="DELETE FROM rutas WHERE id_ruta = '$id_ruta_original' ";
   $resultado1= (mysqli_query ($link, $consulta1) or die ('Consulta fallida 11: ' .mysqli_error($link)));
-  echo "<script > alert('ruta eliminada de la base de datos');window.location='listarRutas.php'</script>";
+  echo "<script > alert('ruta eliminada de la base de datos');window.location='verListadoDeRutas.php'</script>";
 }
 if ($debaja== true){
-  echo '8';
-  echo $id_viaje;
-  $hecho = cancelarViaje($id_viaje);
-    echo '9';
-  $consulta4="UPDATE rutas set debaja ='1' WHERE id_ruta= '$id_ruta_original' " ;
+  $hecho = cancelarViaje($id_viaje, $link);
+    $consulta4="UPDATE rutas set debaja ='1' WHERE id_ruta= '$id_ruta_original' " ;
     $result4=mysqli_query ($link, $consulta4) or die ('Consuluta query4 fallida 14: ' .mysqli_error($link));
-      echo "<script > alert('ruta dada de baja en la bd');window.location='listarRutas.php'</script>";
+      echo "<script > alert('ruta dada de baja en la bd');window.location='verListadoDeRutas.php'</script>";
 }
 
-function cancelarViaje($id_viaje_original){
+function cancelarViaje($id_viaje_original, $link){
 //    $id_viaje_original=$_POST['id_viaje'];
-  echo '1funnnnnnnnnnnnnnnn';
   $consulta0f="SELECT cupo FROM viajes WHERE (id_viaje= '$id_viaje_original')";
-  var_dump($consulta0f);
-  $result0f= (mysqli_query ($link, $consulta0f) or die ('Consuluta query4 fallida 10: ' .mysqli_error($link))) ;
-
-  echo '2funnnnnnnnnnnnnnnn';
-  var_dump($result0f);
-
+  $result0f= mysqli_query ($link, $consulta0f) or die ('Consuluta query4 fallida 10: ' .mysqli_error($link)) ;
   if ($result0f){
       $valoresf = mysqli_fetch_array($result0f);
       //echo '2';
