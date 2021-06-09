530926990ca1ba0d69538a8f3b0748db735d48ec
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-06-09 19:47:00 -0300

    Update cancelarPasaje.php

--- cancelarPasaje.php
+++ cancelarPasaje.php
@@ -14,22 +14,22 @@
     $result0=mysqli_query ($link, $consulta0) or die ('Consuluta query1 fallida 10: ' .mysqli_error($link));
     $consulta1="SELECT mail  FROM usuarios WHERE (id_usuario= '$id')";
     $result1=mysqli_query ($link, $consulta1) or die ('Consuluta query1 fallida 10: ' .mysqli_error($link));
-    echo '1';
+    //echo '1';
     if ($result0){
         $valores = mysqli_fetch_array($result0);
         $valores1= mysqli_fetch_array($result1);
-        echo '2';
+      //  echo '2';
         $pasaje_completo = compararFechas($valores['fecha_hora_salida']);
-        echo 'sali de la funcion';
-        echo $pasaje_completo;
+    //    echo 'sali de la funcion';
+      //  echo $pasaje_completo;
 
         if (!($pasaje_completo)){
         $monto=$valores['precio'] /2;}
         else{
           $monto = $valores['precio'];
         }
-        echo 'dddddd';
-        echo $monto;
+        //echo 'dddddd';
+      //  echo $monto;
 
         $to = $valores1['mail'];
         $subject = "Cancelacion de pasaje en  COMBI 19";
@@ -48,7 +48,7 @@
 
         $consulta4="UPDATE clientes_viajes set estado ='devuelto' WHERE (id_viaje= '$id_viaje_original') and (id_cliente = '$id')" ;
         $result4=mysqli_query ($link, $consulta4) or die ('Consuluta query4 fallida 14: ' .mysqli_error($link));
-        echo 'consilta 4';
+    //    echo 'consilta 4';
         $cupo_nuevo=$valores['cupo'] - 1;
 
         $consulta5="UPDATE viajes SET cupo='$cupo_nuevo' WHERE (id_viaje= '$id_viaje_original') " ;
@@ -56,9 +56,9 @@
       //  echo "result0";
         //  echo "result0 cupo 0";
 
-        echo 'consulta 5';
+      //  echo 'consulta 5';
     if (isset($result4) or isset($result5)){
-      echo 'entra al alert';
+    //  echo 'entra al alert';
       if ($pasaje_completo){
       echo "<script > alert('Pasaje eliminado exitosamente. En un momento le llegara un mail con la confirmación de la cancelación del pasaje y de la devolucion total de su pasaje');window.location='$pagina'</script>";
 }else{
@@ -72,23 +72,23 @@ echo "<script > alert('El pasaje no pudo ser cancelado correctamente. Por favor,
 function compararFechas($fecha_hora_salida){
   echo 'empieza funcion';
         list($fecha, $horaio)= explode(" ", $fecha_hora_salida);
-        echo $fecha, '0000000', $horaio, '111111';
+  //      echo $fecha, '0000000', $horaio, '111111';
         list($hora, $minuto)= explode(":", $horaio);
-        echo $hora, $minuto, '22222222';
+  //      echo $hora, $minuto, '22222222';
         list($anio,$mes,$dia) = explode("-",$fecha);
-        echo $anio, 'tt', $mes, 'tt',  $dia, '333333333';
+    //    echo $anio, 'tt', $mes, 'tt',  $dia, '333333333';
         $anio_dif = $anio - date("Y");
         $mes_dif =  $mes - date("m");
         $dia_dif = $dia - date("d") ;
-        echo 'paso cuentas y pongo dia', date("d"), '     ', $dia, '        ';
+    //    echo 'paso cuentas y pongo dia', date("d"), '     ', $dia, '        ';
     //    $hora_dif = date("H") - $hora;
         $tothoras = 24;
         if ($anio_dif == 0){
-            echo '4';
+          //  echo '4';
           if ($mes_dif == 0) {
-            echo '5';
+          //  echo '5';
             if ($dia_dif <= 2) {
-              echo '6','   ', $dia_dif;
+            //  echo '6','   ', $dia_dif;
               $hora_resta1= $tothoras-date("H");
               if ($dia_dif == 2){
                   $hora_resta1= $hora_resta1 + $tothoras;
@@ -96,7 +96,7 @@ function compararFechas($fecha_hora_salida){
             $hora_suma= $hora_resta1 + $hora;
             if ($hora_suma <= 48){
               $pasaje_completo = false;
-              echo 'es antes de 48 horas';
+            //  echo 'es antes de 48 horas';
             }
             else{
               $pasaje_completo = true;
