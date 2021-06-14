769d05afaf74c13e0fb34a1226d4d0d697c42e28
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-06-14 18:30:48 -0300

    Update validarDatosBusquedaPasaje.php

--- validarDatosBusquedaPasaje.php
+++ validarDatosBusquedaPasaje.php
@@ -137,6 +137,7 @@
                                  <input type="submit" name="cancelar" value="Cancelar pasaje"></input>
                                  <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
                                  <input type="hidden" name="pagina" value="validarDatosBusquedaPasaje.php"></input>
+                                 <input type="hidden" name="ruta" value="<?php echo $origen."-".$destino; ?>"></input>
                             </form>
   <?php                          }
               }  
