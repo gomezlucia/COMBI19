4ea23a60b59d73bd712acc80e89d264e1c4f25d3
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-06-18 20:07:38 -0300

    Update registrarAdicional.php

--- registrarAdicional.php
+++ registrarAdicional.php
@@ -2,9 +2,11 @@
     include("BD.php");// conectar y seleccionar la base de datos
     $link=conectar();
     session_start();
+    if ($_POST['precio'] <> 0){
     if (!empty($_POST['nombre_servicio'] and !empty($_POST['precio']))) {
      	 $servicio=$_POST['nombre_servicio'];
      	 $precio=$_POST['precio'];
+
      	 $existeAdicional="SELECT id_servicio_adicional FROM servicios_adicionales WHERE nombre_servicio='$servicio'";
      	 $resultado= mysqli_query($link,$existeAdicional) or die ('Consulta existe Adicional fallida: ' .mysqli_error($link));
      	 if (mysqli_num_rows($resultado)!=0) {
@@ -18,5 +20,11 @@
          		 	 echo "<script > alert('Adicional registrado exitosamente');window.location='home.php'</script>";
          		 }
          	}
-         }
+        }
+       }else{
+         $_SESSION['nombre_servicio']=$_POST['nombre_servicio'];
+             $_SESSION['precio']=$_POST['precio'];
+         echo "<script > alert('No pueden cargarse servicios adicionales gratuitos en el sistema');window.location='cargarAdicional.php?error=1'</script>";
+       }
+
  ?>
