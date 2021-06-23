692db18d93d0090ff6ed185254cfedb19eab516b
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-06-22 23:50:34 -0300

    Update verPerfilDeUsuario.php

--- verPerfilDeUsuario.php
+++ verPerfilDeUsuario.php
@@ -15,16 +15,28 @@
 </head>
 <body>
  <header>
-       <a href="home.php" >  
-           <img src="logo_is.png" class="div_icono">  
+       <a href="home.php" >
+           <img src="logo_is.png" class="div_icono">
        </a>
        <b><?php echo $nombreUsuario; ?></b>
-<?php           echo menu($id,$link); ?>                       
-       <hr>     
-     </header> 
+<?php           echo menu($id,$link); ?>
+
+<?php $consulta00= "SELECT u.vip FROM usuarios u WHERE u.id_usuario=$id";
+      $resultado00= mysqli_query($link,$consulta00) or die ('Consulta fallida: 00 ' .mysqli_error($link));
+      $es_vip = mysqli_fetch_array($resultado00);
+      // var_dump($es_vip) ;
+      if ($es_vip['vip']=='0'){
+?>
+       <hr>
+     </header>
      <center>
-        <h1>Perfil</h1> 
-     <?php
+        <h1>Perfil</h1>
+     <?php }else{ ?>
+       <hr>
+     </header>
+     <center>
+        <h1>Cliente VIP</h1>
+    <?php }
         $consulta= "SELECT u.nombre, u.apellido, u.nombre_usuario, u.mail, u.fecha_nacimiento, u.DNI FROM usuarios u WHERE u.id_usuario=$id";
         $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: 14 ' .mysqli_error($link));
 
@@ -49,14 +61,26 @@
                         <b>Email:</b> <?php echo $mail;?><br>
                         <b>Fecha de nacimiento:</b> <?php  echo $dia."/".$mes."/".$year;;?><br>
                         <b>D.N.I:</b> <?php echo $DNI;?><br>
-                        <form action="modificarDatosCliente.php" method="post"> 
-                           <input type="submit" value="Modificar datos">
+                        <form action="modificarDatosCliente.php" method="post">
+                           <input type="submit" value="Modificar datos"><br><br>
+                        </form>
+                        <?php if ($es_vip['vip']=='0'){ ?>
+                        <form action="registrarVIP.php" method="post">
+                           <input type="submit" value="Registrarse como cliente VIP"><br><br>
+                            <input type="hidden"name="id" value="<?php echo $id;?>">
+
+                        </form>
+                      <?php }else { ?>
+                        <form action="darDebajaVIP.php" method="post">
+                           <input type="submit" value="Dar de baja membresía " class="btn_buscar" onclick="return SubmitFormulario(this.form)"><br><br>
+                            <input type="hidden"name="id" value="<?php echo $id;?>">
                         </form>
 
+                <?php       }  ?>
                 </p>
               </div>
                    <?php }
-        $consulta= "SELECT t.id_tarjeta, t.numero_tarjeta FROM tarjetas t NATURAL JOIN tarjetas_clientes tc WHERE tc.id_cliente=$id";
+        $consulta= "SELECT t.id_tarjeta, t.numero_tarjeta, tc.vip FROM tarjetas t NATURAL JOIN tarjetas_clientes tc WHERE tc.id_cliente=$id";
         $resultado= mysqli_query($link,$consulta) or die ('Consulta fallida: 14 ' .mysqli_error($link));?>
 
     <h2>Tarjetas</h2>
@@ -65,22 +89,29 @@
             while (($valores = mysqli_fetch_array($resultado)) ){
              $id_tarjeta=$valores['id_tarjeta'];
              $numero_tarjeta = $valores['numero_tarjeta'];
-             $primeros = substr($numero_tarjeta, 0,2); 
-             $ultimos =substr($numero_tarjeta, -4); 
+             $primeros = substr($numero_tarjeta, 0,2);
+             $ultimos =substr($numero_tarjeta, -4);
              $cantidad=(strlen($numero_tarjeta))-strlen($primeros)-strlen($ultimos);
+             $tarj_vip=$valores['vip'];
              ?>
                 <div>
                     <hr>
                     <p>
+            <?php          if ($tarj_vip==1){ ?>
+                  <form action="borrarTarjeta.php" method="post">
+                      <b>Tarjeta asociada a su membresía:</b> <?php echo $primeros.(str_repeat('*',$cantidad)).$ultimos;?>    <input type="hidden"name="id_tarjeta" value="<?php echo $id_tarjeta;?>">
+                        <input type="submit" value="Eliminar">
+                    </form>
 
-                   
-                     <form action="borrarTarjeta.php" method="post"> 
+                    <?php    }else { ?>
+                    <form action="borrarTarjeta.php" method="post">
                        <b>Numero de tarjeta:</b> <?php echo $primeros.(str_repeat('*',$cantidad)).$ultimos;?>    <input type="hidden"name="id_tarjeta" value="<?php echo $id_tarjeta;?>">
                            <input type="submit" value="Eliminar">
                     </form>
+              <?php     }  ?>
                 </p>
               </div>
-            <?php } 
+            <?php }
                 if(mysqli_num_rows($resultado)==0){
             ?>
             <div>
@@ -90,7 +121,7 @@
             <?php
 
     }?>
- <form action="agregarTarjeta.php" method="post"> 
+ <form action="agregarTarjeta.php" method="post">
                            <input type="submit" value="Agregar tarjeta">
                         </form>
                     <?php }?>
