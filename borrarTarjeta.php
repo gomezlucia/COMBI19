0e4620f8117a656ad48294e98d2da98d6130191e
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-06-25 22:56:31 -0300

    Update borrarTarjeta.php

--- borrarTarjeta.php
+++ borrarTarjeta.php
@@ -17,40 +17,9 @@
             if($resultado2){
             echo "<script > alert('La tajeta fue eliminada exitosamente');window.location='verPerfilDeUsuario.php'</script>";
         }
-    }}
-    else{
-      $consulta3= "SELECT id_tarjeta FROM tarjetas_clientes where (id_cliente= $id) " ;
-      $resultado3= mysqli_query($link,$consulta3) or die ('Consulta fallida 22: ' .mysqli_error($link));
-      echo '24';
-      echo $id;
-      echo '----';
-      echo $id_tarjeta;
-      if ($resultado3){
-        echo '26    ';
-      //  var_dump($resultado3);
-        while (($valores = mysqli_fetch_array($resultado3)) and ($aux) ){
-          echo '28';
-          if ($valores['id_tarjeta']<> $id_tarjeta){
-            echo '30';
-            $aux= false;
-            $tarjeta_nueva= $valores['id_tarjeta'];
-            $consulta5="UPDATE tarjetas_clientes SET vip='1' WHERE (id_tarjeta = $tarjeta_nueva) " ;
-              $resultado5 =mysqli_query ($link, $consulta5) or die ('Consulta alta vip fallida: ' .mysqli_error($link));
+    } }
 
-              $consulta11="DELETE FROM tarjetas_clientes WHERE id_tarjeta = $id_tarjeta";
-              $resultado11= (mysqli_query ($link, $consulta11) or die ('Consulta fallida: ' .mysqli_error($link)));
-              if($resultado11){
-              	    $consulta20="DELETE FROM tarjetas WHERE id_tarjeta = $id_tarjeta";
-                      $resultado20= (mysqli_query ($link, $consulta20) or die ('Consulta fallida: ' .mysqli_error($link)));
-                      if($resultado20){
-                      echo "<script > alert('La tajeta fue eliminada exitosamente');window.location='verPerfilDeUsuario.php'</script>";
-                  }
-              }
-          }
-          }
-    }
-  }
-    if((!$resultado) or (!$resultado2)){
+    if((!isset($resultado)) or (!isset($resultado2))) {
     	echo "<script > alert('La tajeta no se pudo eliminar');window.location='verPerfilDeUsuario.php'</script>";
     }
 ?>
