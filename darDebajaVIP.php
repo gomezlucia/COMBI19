056791a83ebaeea65cd834739b55d90fa34dc27c
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-06-25 22:45:48 -0300

    Update darDebajaVIP.php

--- darDebajaVIP.php
+++ darDebajaVIP.php
@@ -13,13 +13,9 @@ $darDebajaVip="UPDATE usuarios SET vip='0' WHERE (id_usuario= '$id_cliente') " ;
 $result_debajaV =mysqli_query ($link, $darDebajaVip) or die ('Consulta darDebajaVip fallida: ' .mysqli_error($link));
 
 if ($result_debajaV){
-  $darDebajaTarjetaVip="UPDATE tarjetas_clientes SET vip='0' WHERE (id_cliente= '$id_cliente') " ;
-  $result_debajaTarjetaV =mysqli_query ($link, $darDebajaTarjetaVip) or die ('Consulta darDebajaTarjetaVip fallida: ' .mysqli_error($link));
-  if ($result_debajaTarjetaV){
+
     echo "<script > alert('Membresia dada de baja correctamente');window.location='verPerfilDeUsuario.php'</script>";
-  }else{
-    echo "<script > alert('Ha surgido un problema.Por favor intente de nuevo mas tarde');window.location='verPerfilDeUsuario.php'</script>";
-  }
+  
 }else{
   echo "<script > alert('Ha surgido un problema.Por favor intente de nuevo mas tarde');window.location='verPerfilDeUsuario.php'</script>";
 }
