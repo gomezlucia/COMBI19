07516f3f5939c6f4e19aed1308dfed581252c1be
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2021-06-28 11:20:39 -0300

    Update comprarPasaje.php

--- comprarPasaje.php
+++ comprarPasaje.php
@@ -6,7 +6,7 @@
      $usuario= new usuario();
      $usuario -> session ($nombreUsuario); //guarda en $nombreUsuario el valor que tiene la sesion (lo pasa por referencia)
      $usuario ->id($id);
-     include "menu.php"; 
+     include "menu.php";
  ?>
  <!DOCTYPE html>
  <html>
@@ -32,13 +32,13 @@
        }
      }
      function ocultarSeleccionarTarjeta(){
-  
+
         var num=document.getElementById("numero_tarjeta").value;
         var clave=document.getElementById("clave").value;
          var selectBox = document.getElementById("tarjetas");
          var fecha=document.getElementById("fecha").value;
        if ( (num != "") || (clave!= "") || (fecha!= "") ){
-       
+
           selectBox.disabled=true;
        }else{
 
@@ -52,9 +52,9 @@
  </head>
   <?php try {
              $usuario -> iniciada($nombreUsuario); //entra al body si el usuario tenia una sesion iniciada
-     ?> 
+     ?>
  <body>
-    <?php 
+    <?php
           $usuarioVip=false;
          	 if (!isset($_POST['id_viaje'])) {
          	 	 $id_viaje=$_SESSION['viaje'];
@@ -78,11 +78,11 @@
            else{
             $tieneTarjetas="SELECT t.numero_tarjeta FROM usuarios c INNER JOIN tarjetas_clientes tc ON (c.id_usuario=tc.id_cliente) INNER JOIN tarjetas t ON (tc.id_tarjeta= t.id_tarjeta) WHERE id_usuario='$id'  and t.fecha_vencimiento>=now()";
            }
-           
+
            $resultadoTarjetas=mysqli_query($link,$tieneTarjetas) or  die ('Consulta tiieneTarjetas fallida: ' .mysqli_error());
 
          	 $viaje="SELECT  r.origen, r.destino, v.fecha_hora_salida, v.fecha_hora_salida, v.fecha_hora_llegada, v.precio FROM viajes v  NATURAL JOIN rutas r  WHERE id_viaje='$id_viaje'";
-         	 $resultadoViaje=mysqli_query($link,$viaje) or  die ('Consulta viaje fallida: ' .mysqli_error()); 
+         	 $resultadoViaje=mysqli_query($link,$viaje) or  die ('Consulta viaje fallida: ' .mysqli_error());
          	 $datosViaje = mysqli_fetch_array($resultadoViaje);
 
 
@@ -92,12 +92,20 @@
                #$volverA=$_GET['p'];
            }?>
                <header>
-       <a href="home.php" >  
-           <img src="logo_is.png" class="div_icono">  
+       <a href="home.php" >
+           <img src="logo_is.png" class="div_icono">
        </a>
-       <b><?php echo $nombreUsuario; ?></b>
-<?php           echo menu($id,$link); ?>                       
-       <hr>     
+       
+        <?php $consulta00= "SELECT u.vip FROM usuarios u WHERE u.id_usuario=$id";
+               $resultado00= mysqli_query($link,$consulta00) or die ('Consulta fallida: 00 ' .mysqli_error($link));
+               $es_vip = mysqli_fetch_array($resultado00);
+               if ($es_vip['vip']=='1'){ ?>
+                 <b><?php echo $nombreUsuario ; ?></b> <b>VIP</b>
+         <?php } else{    ?>
+           <b><?php echo $nombreUsuario; ?></b>
+<?php      }    echo menu($id,$link);
+     ?>
+       <hr>
      </header>
          	 <center>
          	 	 <p> <b>Origen:</b> <?php echo $datosViaje['origen'];?><br>
@@ -106,7 +114,7 @@
                    <b>Fecha y hora de llegada:</b> <?php echo date("d/m/Y  H:i:s", strtotime($datosViaje['fecha_hora_llegada']));?><br>
                    <?php if(!empty($_POST['adicionales_seleccionados'])){
                          ?><b>Servicios adicionales:</b> <?php echo str_replace ( "," , " $" , $_POST['adicionales_seleccionados']);?><br><?php
-                   } 
+                   }
                     $esVip="SELECT c.vip FROM usuarios c  Where id_usuario='$id'";
                     $resultado=mysqli_query($link,$esVip) or  die ('Consulta vip fallida: ' .mysqli_error());
                     $cliente=mysqli_fetch_array($resultado);
@@ -117,13 +125,13 @@
                       <b>Total a abonar:</b><del> <?php echo '$' . $total." " ;?></del> <b style="color:#FF0000";><?php echo $descontado;  ?></b>   <?php
                  }
                    else{?>
-                     <b>Total a abonar:</b> <?php echo '$' . $total;?><br> <?php 
+                     <b>Total a abonar:</b> <?php echo '$' . $total;?><br> <?php
                  }?>
-                   <form action="validarPago.php" method="post">  
+                   <form action="validarPago.php" method="post">
                     <div id="seleccionarT">
 
-          <?php    
-                     
+          <?php
+
                        if(mysqli_num_rows($resultadoTarjetas)!=0){
                         if( (!empty($numero_tarjeta)) or (!empty($fecha)) ){ //significa que volvio del validar pago con una tarjeta ingresada en el input
                           ?>
@@ -143,14 +151,14 @@
              }
                           while ($tarjetas = mysqli_fetch_array($resultadoTarjetas)) {
                              echo '<option value="' . $tarjetas["numero_tarjeta"] . '">' . $tarjetas["numero_tarjeta"] . '</option>';
-                           } ?> 
+                           } ?>
                             </select> <br><br>
  <?php     }
-  }?> 
+  }?>
                      </div>
                       <div id="ingresarT">
                      <?php if(!empty($tarjetas_select)){
-                                           ?> 
+                                           ?>
                      <b>Numero de tarjeta: </b><input type="text" name="numero_tarjeta"  maxlength="16"  disabled="" onchange="ocultarSeleccionarTarjeta()" id="numero_tarjeta"> </input><br><br>
                      <b>Clave de seguridad: </b><input type="password" name="clave" id="clave" maxlength="4" disabled="" onchange="ocultarSeleccionarTarjeta()" value=""></input><br><br>
                      <b>Fecha de vencimiento: </b><input type="month" name="fecha" id="fecha" disabled="" class="form-control" onchange="ocultarSeleccionarTarjeta()"><br><br>
@@ -158,36 +166,35 @@
 
                      <?php }
                      else{
-                       ?> 
+                       ?>
                      <b>Numero de tarjeta: </b><input type="text" name="numero_tarjeta"  maxlength="16" value="<?php echo $numero_tarjeta ?>"  onchange="ocultarSeleccionarTarjeta()" id="numero_tarjeta"> </input><br><br>
                      <b>Clave de seguridad: </b><input type="password" name="clave" id="clave" maxlength="4"  value="" onchange="ocultarSeleccionarTarjeta()"></input><br><br>
                      <b>Fecha de vencimiento: </b><input type="month" name="fecha" id="fecha" value="<?php echo $fecha ?>" class="form-control" onchange="ocultarSeleccionarTarjeta()"><br><br>
                      <?php
                      } ?>
                      </div>
-                          <input type="hidden" name="adicionales_seleccionados" value="<?php echo $adicionales_seleccionados; ?>"></input> 
-                          <input type="hidden" name="usuarioVip" value="<?php echo $usuarioVip; ?>"></input> 
+                          <input type="hidden" name="adicionales_seleccionados" value="<?php echo $adicionales_seleccionados; ?>"></input>
+                          <input type="hidden" name="usuarioVip" value="<?php echo $usuarioVip; ?>"></input>
                            <input type="hidden" name="total" value="<?php echo $total; ?>"></input>
                           <input type="hidden" name="id_viaje" value="<?php echo $id_viaje; ?>"></input>
-                          <input type="button" name="Cancelar" value="Cancelar" onClick="location.href='home.php'"> 
-                          <input type="submit" name="Submit" value="Pagar pasaje"> 
+                          <input type="button" name="Cancelar" value="Cancelar" onClick="location.href='home.php'">
+                          <input type="submit" name="Submit" value="Pagar pasaje">
                           <input type="hidden" name="precio" value="<?php echo  $datosViaje['precio']; ?>">
                           <input type="hidden" name="volverA" value="<?php echo  $volverA;?>">
-                          
-              
+
+
                      </form>
          	 </center>
- <?php  
-    
+ <?php
+
     ?>
- 
+
  </body>
  <?php   } catch (Exception $e) { //entra a esta parte solo si no tenia una sesion iniciada
-                 $mensaje=$e->getMessage(); 
+                 $mensaje=$e->getMessage();
                  echo "<script> alert('Por favor, inicie sesión en COMBI-19 o regístrese como nuevo usuario para realizar la compra');window.location='/COMBI19-main/inicioSesion.php'</script>";
                   //redirige a la pagina inicioSesion y muestra una mensaje de error
          }?>
 </html>
-
 
           
