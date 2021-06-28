4dbeab120c295ee44da216161feb771223cecefd
lujan2001 <81389053+lujan2001@users.noreply.github.com>
2021-06-28 09:44:34 -0300

    Update venderExistente.php

--- venderExistente.php
+++ venderExistente.php
@@ -8,7 +8,11 @@ $usuario ->id($id);
 if (isset($_POST['id_usuario'])) {
 	 $vender="INSERT INTO clientes_viajes( id_cliente, id_viaje, estado, total) VALUES ('$_POST[id_usuario]',$_POST[id_viaje],'en curso','$_POST[precio]')";
 	 $resultadoVender= mysqli_query($link,$vender) or die ('Consulta vender fallida: ' .mysqli_error($link));
-	 if ($resultadoVender) {
+	 
+	 $actualizarCupo="UPDATE viajes SET cupo=cupo+1 WHERE id_viaje='$_POST[id_viaje]'";
+     $resultadoActualizar=mysqli_query($link,$actualizarCupo) or  die ('Consulta actualizarCupo fallida: ' .mysqli_error()); 
+	 
+	 if ($resultadoVender and $resultadoActualizar) {
 	 	 echo "<script > alert('Pasaje vendido');window.location='proximoViaje.php'</script>";
 	 }
 }
